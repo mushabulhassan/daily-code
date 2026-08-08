819f5b37424dba2bd968937bacd77f75aeacd931
Mashab Ul Hassan <mushabulhassan@gmail.com>
2026-08-08 05:42:56 +0500

    \Code managing

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,4 +1,4 @@
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons  from "@expo/vector-icons";
 import { ImageBackground } from "expo-image";
 import { useState } from "react";
 import {
